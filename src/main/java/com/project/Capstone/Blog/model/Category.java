5ba57f08b6f8c603c804f50dcff0173cac71467f
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-07-18 18:57:13 +0530

    Create Category.java

---
+++ src/main/java/com/project/Capstone/Blog/model/Category.java
@@ -0,0 +1,18 @@
+package com.project.Capstone.blog.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
