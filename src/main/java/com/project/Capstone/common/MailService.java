684676214d441a0897476357d454a78958cc768a
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 14:02:53 +0530

    Create MailService.java

---
+++ src/main/java/com/project/Capstone/common/MailService.java
@@ -0,0 +1,28 @@
+package com.project.Capstone.common;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendMail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+            log.info("Email sent successfully to {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}. Error: {}", to, e.getMessage());
+        }
+    }
+}
