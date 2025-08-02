e35d878a0489d15546c49a25e005ecf28c58b69d
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 13:48:54 +0530

    Update PostMapper.java

--- src/main/java/com/project/Capstone/Blog/mapper/PostMapper.java
+++ src/main/java/com/project/Capstone/Blog/mapper/PostMapper.java
@@ -1,8 +1,21 @@
 package com.project.Capstone.blog.mapper;
 
+import com.project.Capstone.blog.dto.response.PostResponseDto;
 import com.project.Capstone.blog.model.Post;
-import com.project.Capstone.blog.dto.response.PostResponse;
+import com.project.Capstone.blog.model.Tag;
 
-public interface PostMapper {
-    PostResponse toResponse(Post post);
+public class PostMapper {
+    public static PostResponseDto toDto(Post post) {
+        return PostResponseDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .authorName(post.getAuthor().getName())
+                .authorEmail(post.getAuthor().getEmail())
+                .categoryName(post.getCategory().getName())
+                .tags(post.getTags().stream().map(Tag::getName).toList())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .build();
+    }
 }
