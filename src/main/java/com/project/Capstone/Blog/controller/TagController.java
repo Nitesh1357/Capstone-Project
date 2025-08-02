a2dccabb2c7a7204258184ad7461d5ff862eaefe
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 13:37:14 +0530

    Update TagController.java

--- src/main/java/com/project/Capstone/Blog/controller/TagController.java
+++ src/main/java/com/project/Capstone/Blog/controller/TagController.java
@@ -5,11 +5,12 @@ import com.project.Capstone.blog.dto.response.TagResponse;
 import com.project.Capstone.blog.service.TagService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/blog/tags")
 @RequiredArgsConstructor
@@ -17,20 +18,27 @@ import java.util.List;
 public class TagController {
 
     private final TagService tagService;
-
     @PostMapping
     public ResponseEntity<TagResponse> create(@RequestBody TagRequest request) {
-        return ResponseEntity.ok(tagService.create(request));
+        log.info("Creating new tag with name: {}", request.getName());
+        TagResponse response = tagService.create(request);
+        log.info("Tag created successfully with ID: {}", response.getId());
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping
     public ResponseEntity<List<TagResponse>> getAll() {
-        return ResponseEntity.ok(tagService.getAll());
+        log.info("Fetching all blog tags");
+        List <TagResponse> tags = tagService.getAll();
+        log.info ("Total tags fetched : {} ", tags.size());
+        return ResponseEntity.ok(tags);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Delete tag with id id:{}", id);
         tagService.delete(id);
+        log.info("Tag deleted Successfully with Id: {}", id);
         return ResponseEntity.noContent().build();
     }
 }
