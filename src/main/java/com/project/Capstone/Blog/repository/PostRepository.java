306903ddda35e07ab0aced217b58fc17a9ffcdcc
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 13:56:53 +0530

    Update PostRepository.java

--- src/main/java/com/project/Capstone/Blog/repository/PostRepository.java
+++ src/main/java/com/project/Capstone/Blog/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.project.Capstone.blog.repository;
 
-import com.project.Capstone.blog.model.Category;
+import com.project.Capstone.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
