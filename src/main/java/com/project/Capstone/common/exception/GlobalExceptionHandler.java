ab0329e994b428e04f349215a8cc826e4f07280b
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 14:02:01 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/project/Capstone/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.project.Capstone.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // You can add more handlers for validation, JWT, etc.
+}
