2153ab00a7c0f0fde83183aeb167cd7c67a0c348
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-02 13:49:09 +0530

    Update PostMapperImpl.java

--- src/main/java/com/project/Capstone/Blog/mapper/impl/PostMapperImpl.java
+++ src/main/java/com/project/Capstone/Blog/mapper/impl/PostMapperImpl.java
@@ -1,19 +1,33 @@
 package com.project.Capstone.blog.mapper.impl;
 
-import com.project.Capstone.blog.dto.response.PostResponse;
-import com.project.Capstone.blog.mapper.PostMapper;
+import com.project.Capstone.blog.dto.request.PostRequestDto;
+import com.project.Capstone.blog.dto.response.PostResponseDto;
+import com.project.Capstone.blog.model.BlogUser;
 import com.project.Capstone.blog.model.Post;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostMapperImpl implements PostMapper {
-    @Override
-    public PostResponse toResponse(Post post) {
-        PostResponse response = new PostResponse();
-        response.setId(post.getId());
-        response.setTitle(post.getTitle());
-        response.setContent(post.getContent());
-        response.setCreatedAt(post.getCreatedAt());
-        return response;
+public class PostMapperImpl {
+
+    public PostResponseDto toDto(Post post) {
+        BlogUser author = post.getAuthor();
+
+        return PostResponseDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
+                .authorName(author.getName())
+                .authorEmail(author.getEmail())
+                .build();
+    }
+
+    public Post toEntity(PostRequestDto dto, BlogUser author) {
+        return Post.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .author(author)
+                .build();
     }
 }
