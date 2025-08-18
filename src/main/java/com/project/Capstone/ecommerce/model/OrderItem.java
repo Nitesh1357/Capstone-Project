bb8aca4bd9f1393d531da7a723b2c1bd8abe61aa
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-18 13:34:25 +0530

    Create OrderItem.java

---
+++ src/main/java/com/project/Capstone/ecommerce/model/OrderItem.java
@@ -0,0 +1,21 @@
+package com.project.Capstone.ecommerce.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
+
