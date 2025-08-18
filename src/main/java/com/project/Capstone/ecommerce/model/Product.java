d89444619a149cc23aa3a7443affe9b94c2f9daf
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-18 13:35:07 +0530

    Create Product.java

---
+++ src/main/java/com/project/Capstone/ecommerce/model/Product.java
@@ -0,0 +1,25 @@
+package com.project.Capstone.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
