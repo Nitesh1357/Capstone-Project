a50be37136528e6f87693271fdcba46d9cebb406
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-08-18 13:38:24 +0530

    Create OrderRepository.java

---
+++ src/main/java/com/project/Capstone/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.project.Capstone.ecommerce.repository;
+
+import com.project.Capstone.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
