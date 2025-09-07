8f3cf9ad59d3456f3c994ab8310e1db8d7b52e44
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2025-09-07 14:43:21 +0530

    Create ReplyRepository.java

---
+++ src/main/java/com/project/Capstone/ supportticket/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.project.Capstone.supportticket.repository;
+
+import com.project.Capstone.supportticket.model.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
